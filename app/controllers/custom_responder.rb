ad3bfc3541c8b32ddd7d7c4f75ae76c8da36fad6
macournoyer <macournoyer@gmail.com>
2013-07-31 12:32:42 -0400

    Custom responder to set Flash.

--- app/controllers/custom_responder.rb
+++ app/controllers/custom_responder.rb
@@ -1,3 +1,13 @@
 class CustomResponder < ActionController::Responder
-  # ...
+  def to_html
+    unless has_errors?
+      if post? # create
+        controller.flash.notice = resource.class.name.humanize + " successfuly created."
+      elsif patch? # updatem in Rails < 4: PUT, Rails => 4.0: PATCH
+        controller.flash.notice = resource.class.name.humanize + " successfuly updated."
+      end
+    end
+
+    super
+  end
 end
