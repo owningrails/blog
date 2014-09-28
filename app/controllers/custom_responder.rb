4e0ae72c4480047370ba872be6116d2c17470789
macournoyer <macournoyer@gmail.com>
2014-09-28 16:11:56 -0400

    Implement a custom responder.

--- app/controllers/custom_responder.rb
+++ app/controllers/custom_responder.rb
@@ -1,3 +1,13 @@
 class CustomResponder < ActionController::Responder
-  # ...
+  def to_html
+    unless has_errors?
+      if post? # create
+        controller.flash.notice = resource.class.name.humanize + " created successfuly"
+      elsif patch? || put? # update
+        controller.flash.notice = resource.class.name.humanize + " updated successfuly"
+      end
+    end
+
+    super
+  end
 end
